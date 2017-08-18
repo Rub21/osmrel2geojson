3c9966b31ff9849493b4fa35297fa7aeecbd0579
Rub21 <ruben@mapbox.com>
2017-08-17 23:19:04 -0500

    convert to geojson
    e

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# osmrel2geojson
+# OpenStreetMap relation --> geojson
+
+## Install
+
+```
+git clone https://github.com/Rub21/osmrel2geojson.git
+cd osmrel2geojson/
+npm link
+```
+## Usage
+
+```
+osmrel2geojson <pbf File> <out.geojson File> <options>
+```
+
+e.g
+
+```
+osmrel2geojson peru-latest.osm.pbf geo.geojson --type=restriction
+
+```
---
+++ index.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+'use strict';
+var fs = require('fs');
+var argv = require('minimist')(process.argv.slice(2));
+var path = require('path');
+var readline = require('readline');
+var togeojson = require('./src');
+var geojson = {
+	"type": "FeatureCollection",
+	"features": []
+};
+var opts = {};
+for (var tag in argv) {
+	if (tag !== '_') {
+		opts[tag] = argv[tag];
+	}
+}
+var inputFile = argv._[0];
+var outputFile = argv._[1];
+var tempFile = outputFile.split('.')[0] + '.json';
+var wstream = fs.createWriteStream(outputFile);
+
+togeojson(opts, inputFile, tempFile, function() {
+	var rd = readline.createInterface({
+		input: fs.createReadStream(tempFile),
+		output: process.stdout,
+		terminal: false
+	});
+	rd.on('line', function(line) {
+		var obj = JSON.parse(line);
+		if (obj.type === 'FeatureCollection') {
+			geojson.features = geojson.features.concat(obj.features);
+		} else {
+			geojson.features = geojson.features.concat(obj);
+		}
+	}).on('close', function() {
+		wstream.write(JSON.stringify(geojson));
+		fs.unlink(tempFile);
+		wstream.end();
+	});
+});
